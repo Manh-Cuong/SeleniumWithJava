762255faaaf93f4c1022794ac300b0d9c224a8cf
cuonglm94 <manhcuong97kma@gmail.com>
2024-01-30 21:17:18 +0700

    Init day7

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TextBoxTest tb= new TextBoxTest();
-		tb.SubmitSuccessfully();
+		//tb.SubmitSuccessfully();
+		//tb.EmailFormatIsWrong();
+		
+		CheckBox cb= new CheckBox();
+		cb.VerifyCheckBoxCorrectly();
 
 	}
 
